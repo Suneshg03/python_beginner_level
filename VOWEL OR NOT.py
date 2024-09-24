0deba6fdc4ec4b95baf6e61ed600cb3db059d9e0
Suneshg03 <sunesh.g03@gmail.com>
2024-09-24 11:39:37 +0530

    Add files via upload

---
+++ VOWEL OR NOT.py
@@ -0,0 +1,34 @@
+#VOWELS OR NOT
+#METHOD 1: CHARACTER IS VOWEL PR NOT
+
+a=input("ENTER ANY ALPHABET LETTER:")
+vowels="aeouiAEOUI"
+if a in vowels:
+    print(a,"IS A VOWEL")
+
+else:
+    print(a,"IS NOT A VOWEL")
+
+
+
+#METHOD 2: WORD IS VOWEL OR NOT
+a=input("ENTER ANY CHARACTERS:")
+vowels="aeouiAEOUI"
+for i in a:
+    if i in vowels:
+        print(i,"VOWEL")
+
+    else:
+        print(i,"NOT VOWELS")
+
+
+#METHOD 3
+a=input("ENTER YOUR CHARACTER:")
+if (a=="a" or a=="e" or a=="o" or a=="u" or a=="i"):
+    print(a,"is vowel")
+
+elif (a=="A" or a=="E" or a=="O" or a=="U" or a=="I"):
+    print(a,"is vowel")
+
+else:
+    print(a,"is consonant")
