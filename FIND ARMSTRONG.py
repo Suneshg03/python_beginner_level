5f7c0753f67497ca570cccfa72f0a1d437f1ea38
Suneshg03 <sunesh.g03@gmail.com>
2024-09-24 11:30:58 +0530

    Add files via upload

---
+++ FIND ARMSTRONG.py
@@ -0,0 +1,14 @@
+#FIND ARMSTRONG
+a=int(input("ENTER THE VALUE:"))
+b=str(a)
+c=len(b)
+d=0
+for i in b:
+    d=d+int(i)**c
+
+if a==d:
+    print(a,"is armstrong")
+
+else:
+    print(a,"is not armstrong")
+    
