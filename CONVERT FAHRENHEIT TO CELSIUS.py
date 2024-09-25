c21dfaa44c26ef451f967bb9f8ab4d4b0b6340d1
Suneshg03 <sunesh.g03@gmail.com>
2024-09-25 11:42:19 +0530

    Add files via upload

---
+++ CONVERT FAHRENHEIT TO CELSIUS.py
@@ -0,0 +1,10 @@
+#CONVERT FAHRENHEIT TO CELSIUS
+fahrenheit=int(input("ENTER FAHRENHEIT:"))
+celsius=(fahrenheit-32)*5/9
+print(f"the celsius is:{celsius:.1f}")
+
+
+#CONVERT CELSIUS TO FAHRENHEIT
+celsius=int(input("ENTER CELSIUS:"))
+fahrenheit=(celsius*9/5)+32
+print(f"the fahrenheit is:{fahrenheit:.1f}")
