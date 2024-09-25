4cd24d0ccf3299a4b4607fc29afe43717dc6de42
Suneshg03 <sunesh.g03@gmail.com>
2024-09-25 11:21:16 +0530

    Add files via upload

---
+++ KILOMETERS TO MILES CONVERTER.py
@@ -0,0 +1,9 @@
+#KM TO MILES
+km=int(input("ENTER A KM:"))
+miles=km*0.621371
+print(f"the miles:{miles:.2f}")
+
+#MILES TO KM
+miles=int(input("ENTER A MILES:"))
+km=miles*1.60934
+print(f"the km is:{km:.2f}")
