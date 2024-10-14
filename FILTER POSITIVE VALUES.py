2a0713f189f7e4befb446d15dc9cd6031f86cc1c
Suneshg03 <sunesh.g03@gmail.com>
2024-10-14 21:03:33 +0530

    Add files via upload

---
+++ FILTER POSITIVE VALUES.py
@@ -0,0 +1,11 @@
+#FILTER POSITIVE VALUES
+
+lst=[1,2,3,0,-2,-1,10]
+lst1=[]
+for i in lst:
+    if i>0:
+        lst1.append(i)
+print("After the filtering positive numbers are:",lst1)
+
+
+        
